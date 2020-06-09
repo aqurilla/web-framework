3634bb0fcfa4fc2ff9857e2182db0c80755e6b74
aqurilla <aqurilla@gmail.com>
2020-06-08 22:57:58 -0700

    extracted View class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const userForm = new UserForm(
-  document.getElementById('root'),
-  User.buildUser({ name: 'NAME', age: 50 })
-);
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(
+    root,
+    User.buildUser({ name: 'NAME', age: 50 })
+  );
+
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
---
+++ src/views/View.ts
@@ -0,0 +1,36 @@
+import { Model } from '../models/Model';
+
+export abstract class View<T extends Model<K>, K> {
+  constructor(public parent: Element, public model: T) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+
+  abstract eventsMap(): { [key: string]: () => void };
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  abstract template(): string;
+
+  render(): void {
+    this.parent.innerHTML = ``;
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+    this.parent.append(templateElement.content);
+  }
+}
